61c8587cc449cb0d0d0b6aaa499a524a9133fbca
Basistransformoptimusprime <valentin.wyss@bluewin.ch>
2021-09-06 09:32:51 +0200

    commiting minor changes before new branch

--- Special_States.py
+++ Special_States.py
@@ -51,11 +51,40 @@ class Bouncing_Gaussian(pib.Particle_in_Box_Immediate_Mode):
         states_to_add = list(l_range)
         self.add_state(states_to_add, amplitudes_to_add)
 
+    @staticmethod
+    def estimate_l_0(case, L, k_0) -> int:
+        """THIS FUNCTION IS EXPERIMENTAL AND WILL BE LATER INCLUDED IN BOUNDARY
+        CLASSES
+        """
+        if case == "neumann":
+            return int(L/np.pi*k_0)
+        elif case == "dirichlet_neumann":
+            return int(L/np.pi*k_0 - 0.5)
+        elif case == "dirichlet":
+            return int(L/np.pi*k_0 - 1)
 
     def __init__(self, case: str, L: float, m: float, l_0: int, l_range: int, a: float) -> None:
         super().__init__(case, L, m)
+        
         self._l_0 = l_0
-        self._l_range = l_range
         self._k_0 = self._sp.boundary_lib.get_kl(l_0)
+        
+        self._l_range = l_range
         self._k_space_gaussian = Momentum_Space_Gaussian(a, self._k_0)
         self.find_amplitudes()
+
+    @classmethod
+    def init_with_k_0(cls, case: str, L: float, m: float, k_0: int, l_range: int, a: float) -> Bouncing_Gaussian:
+        """THIS IS NOT AN ELEGANT SOLUTION!
+        """
+        l_0 = cls.estimate_l_0(case, L, k_0)
+        Gaussian = cls(case, L, m, l_0, l_range, a)
+        Gaussian._l_0 = l_0
+        Gaussian._k_0 = k_0
+        Gaussian._k_space_gaussian = Momentum_Space_Gaussian(a, k_0)
+        Gaussian.find_amplitudes()
+        return Gaussian
+        
+
+
+        
