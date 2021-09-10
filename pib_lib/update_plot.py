eeba6621250955ef48f58f2eda4e2a95d7854d58
Basistransformoptimusprime <valentin.wyss@bluewin.ch>
2021-09-10 20:02:38 +0200

    mostly completed basic ploting functionality

--- pib_lib/update_plot.py
+++ pib_lib/update_plot.py
@@ -16,6 +16,9 @@ light_red = cs.hls_to_rgb(0, 0.87, 1)
 mid_red = cs.hls_to_rgb(0, 0.67, 1)
 dark_red = cs.hls_to_rgb(0, 0.37, 1)
 
+def revival_time(state: pib.Particle_in_Box_State) -> float:
+    return 4*state.m*state.L**2/np.pi
+
 class Updatable_Plot(ABC):
     """This class provides basic visualization functionality (based on 
     matplotlib) for the particle-in-a-box states. For more advanced or better
@@ -29,24 +32,28 @@ class Updatable_Plot(ABC):
         self._ls = "-"
         self._lw = 1
 
-    def plot_config(self, color = dark_blue, linestyle = "-", linewidth = 1):
+    def plot_config(self, color = dark_blue, linestyle = "-", linewidth = 1) -> Updatable_Plot:
         self._color = color
         self._ls = linestyle
         self._lw = linewidth
         return self
 
+    @abstractmethod
     def plot(self) -> plt.Line2D:
         pass
 
     def update(self) -> None:
         pass
-
+    
+    @abstractmethod
     def _anim_t(self) -> Callable[[int], List[plt.Line2D]]:
         pass
-
+    
+    @abstractmethod
     def _anim_L(self) -> Callable[[int], List[plt.Line2D]]:
         pass
 
+    @abstractmethod
     def _anim_gamma(self) -> Callable[[int], List[plt.Line2D]]:
         pass
 
@@ -76,52 +83,36 @@ class Single_Updatable_Plot(Updatable_Plot):
         super().__init__(fig)
         self._res = 300
         self._state = state
+        self._time = 0
+        self._lines = None
         if not axis:
             self._axis = fig.add_subplot()
         else:
             self._axis = axis
 
-class Single_Updatable_Line(Single_Updatable_Plot):
-    def __init__(self, state: pib.Particle_in_Box_State, mode: str, fig: plt.Figure, axis: plt.Axes=None) -> None:
-        super().__init__(state, fig, axis=axis)
-        self._mode = mode
-        self._time = 0
-        self._lines = None
-
-    def update(self) -> None:
-        self._x = None
-        self._y = None
-
-    def set_t(self, time):
+    def set_t(self, time) -> Single_Updatable_Plot:
         self._time = time
         return self
-    
+
+    def _line_data_updater(self) -> None:
+        pass
+
+    def _line_data_init(self) -> None:
+        pass
+
     def plot(self) -> plt.Line2D:
         self.update()
         if self._lines:
-            self._lines[0].set_data(self._x, self._y(self._time))
+            self._line_data_updater()
         else:
-            self._lines = self._axis.plot(self._x, self._y(self._time), color=self._color, linestyle=self._ls, linewidth=self._lw, animated=True)
-        return self._lines[0]
-
+            self._line_data_init()
+        
     def _anim_t(self, start, system_time_per_frame) -> Callable[[int], List[plt.Line2D]]:
-        self._t = start
+        self._time = start
         self.plot()
         def anim(i):
-            t = start+i*system_time_per_frame
-            self._lines[0].set_data(self._x, self._y(t))
-            return self._lines
-        return anim
-
-    def _anim_L(self, start, system_L_per_frame) -> Callable[[int], List[plt.Line2D]]:
-        self._state.L = start
-        self.plot()
-        def anim(i):
-            L = start+i*system_L_per_frame
-            self._state.L = L
-            self._axis.set_xlim([-L/2, L/2])
-            self.update()
-            self._lines[0].set_data(self._x, self._y(self._time))
+            self._time = start+i*system_time_per_frame
+            self._line_data_updater()
             return self._lines
         return anim
 
@@ -129,11 +120,38 @@ class Single_Updatable_Line(Single_Updatable_Plot):
         self._state.gamma = start
         self.plot()
         def anim(i):
-            gamma = start+i*system_gamma_per_frame
-            self._state.gamma = gamma
-            self._lines[0].set_data(self._x, self._y(self._time))
+            self._state.gamma = start+i*system_gamma_per_frame
+            self._line_data_updater()
             return self._lines
         return anim
+
+    def _anim_L(self, start, system_L_per_frame) -> Callable[[int], List[plt.Line2D]]:
+        self._state.L = start
+        self.plot()
+        def anim(i):
+            self._state.L = start+i*system_L_per_frame
+            self._line_data_updater()
+            return self._lines
+        return anim
+
+class Single_Updatable_Line(Single_Updatable_Plot):
+    def __init__(self, state: pib.Particle_in_Box_State, mode: str, fig: plt.Figure, axis: plt.Axes=None) -> None:
+        super().__init__(state, fig, axis=axis)
+        self._mode = mode
+
+    def update(self) -> None:
+        self._x = None
+        self._y = None
+
+    def _line_data_init(self) -> None:
+        self._lines = self._axis.plot(self._x, self._y(self._time), color=self._color, linestyle=self._ls, linewidth=self._lw, animated=True)
+    
+    def _line_data_updater(self) -> None:
+        self._lines[0].set_data(self._x, self._y(self._time))
+
+    def plot(self) -> plt.Line2D:
+        super().plot()
+        return self._lines[0]
 
 class Pos_Space_Plot(Single_Updatable_Line):
     def __init__(self, state: pib.Particle_in_Box_State, mode: str, fig: plt.Figure, axis: plt.Axes=None) -> None:
@@ -151,127 +169,72 @@ class Pos_Space_Plot(Single_Updatable_Line):
 class Momentum_Space_Plot(Single_Updatable_Line):
     def __init__(self, state: pib.Particle_in_Box_State, mode: str, fig: plt.Figure, axis: plt.Axes=None) -> None:
         super().__init__(state, mode, fig, axis=axis)
-        self._bound = 15
+        self._n_bound = 15
 
-    def set_bound(self, new_bound):
-        self._bound = new_bound
+    def set_bound(self, new_bound) -> Momentum_Space_Plot:
+        self._n_bound = new_bound
         return self
 
     def update(self) -> None:
-        self._k = np.linspace()
+        k_bound = self._state.boundary_lib.get_kn(self._n_bound)
+        self._x = np.linspace(-k_bound, k_bound, self._res)
+        if self._mode=="abs_square":
+            self._y = lambda t: np.abs(self._state.k_space_wavefunction(self._x, t))**2
+        elif self._mode=="real":
+            self._y = lambda t: np.real(self._state.k_space_wavefunction(self._x, t))
+        elif self._mode=="imag":
+           self._y = lambda t: np.imag(self._state.k_space_wavefunction(self._x, t))
 
-class Epextation_Value_Line(Single_Updatable_Plot):
-    def __init__(self, state: pib.Particle_in_Box_State, mode: str, fig: plt.Figure, axis: plt.Axes=None) -> None:
-        super().__init__(state, mode, fig, axis=axis)
+class Expectation_Value_Line(Single_Updatable_Plot):
+    def __init__(self, state: pib.Particle_in_Box_State, fig: plt.Figure, axis: plt.Axes=None) -> None:
+        super().__init__(state, fig, axis=axis)
         self._time = 0
         self._line = None
 
     def update(self) -> None:
         self._expectation_value = None
 
-    def set_t(self, time):
-        self._time = time
-        return self
+    def _line_data_init(self) -> None:
+        self._line = self._line = self._axis.axvline(self._expectation_value(self._time), color=self._color, linewidth=self._lw, linestyle=self._ls, animated=True)
+        self._lines = [self._line]
+    
+    def _line_data_updater(self) -> None:
+        expectation_value_position = self._expectation_value(self._time)
+        self._line.set_data([expectation_value_position, expectation_value_position],[0,1])
+        self._lines = [self._line]
 
     def plot(self) -> plt.Line2D:
-        self.update()
-        expectation_value_position = self._y(self._time)
-        if self._line:
-            self._line.set_data([expectation_value_position, expectation_value_position],[0,1])
-        else:
-            self._line = self._axis.axvline(expectation_value_position, color=self._color, linewidth=self._lw, linestyle=self._ls)
+        super().plot()
         return self._line
-
-    def _anim_t(self, start, system_time_per_frame) -> Callable[[int], List[plt.Line2D]]:
-        self._time = start
-        self.update()
-        def anim(i):
-            t = start+i*system_time_per_frame
-            expectation_value_position = self._y(t)
-            self._line.set_data([expectation_value_position, expectation_value_position],[0,1])
-            return [self._line]
-        return anim
-
-"""
-class Pos_Space_Plot(Single_Updatable_Plot):
-    def __init__(self, state: pib.Particle_in_Box_State, mode: str, fig: plt.Figure, axis: plt.Axes=None) -> None:
+  
+class Pos_Expectation_Value(Expectation_Value_Line):
+    def __init__(self, state: pib.Particle_in_Box_State, fig: plt.Figure, axis: plt.Axes=None) -> None:
         super().__init__(state, fig, axis=axis)
-        self._mode = mode
-        self._time = 0
-        self._lines = None
 
     def update(self) -> None:
-        self._x = np.linspace(-self._state.L/2, self._state.L/2, self._res)
-        if self._mode=="modulus_square":
-            self._y = lambda t: np.abs(self._state.x_space_wavefunction(self._x, t)**2)
-        elif self._mode=="real":
-            self._y = lambda t: np.real(self._state.x_space_wavefunction(self._x, t))
-        elif self._mode=="imag":
-           self._y = lambda t: np.imag(self._state.x_space_wavefunction(self._x, t))
+        self._expectation_value = lambda t: self._state.x_space_expectation_value(t)
 
-    def set_t(self, time):
-        self._time = time
-        return self
-    
-    def plot(self) -> plt.Line2D:
-        self.update()
-        if self._lines:
-            self._lines[0].set_data(self._x, self._y(self._time))
-        else:
-            self._lines = self._axis.plot(self._x, self._y(self._time), color=self._color, linestyle=self._ls, linewidth=self._lw, animated=True)
-        return self._lines[0]
+class Momentum_Expectation_Value(Expectation_Value_Line):
+    def __init__(self, state: pib.Particle_in_Box_State, fig: plt.Figure, axis: plt.Axes=None) -> None:
+        super().__init__(state, fig, axis=axis)
 
-    def _anim_t(self, start, system_time_per_frame) -> Callable[[int], List[plt.Line2D]]:
-        self._t = start
-        self.plot()
-        def anim(i):
-            t = start+i*system_time_per_frame
-            self._lines[0].set_data(self._x, self._y(t))
-            return self._lines
-        return anim
-
-    def _anim_L(self, start, system_L_per_frame) -> Callable[[int], List[plt.Line2D]]:
-        self._state.L = start
-        self.plot()
-        def anim(i):
-            L = start+i*system_L_per_frame
-            self._state.L = L
-            self._axis.set_xlim([-L/2, L/2])
-            self.update()
-            self._lines[0].set_data(self._x, self._y(self._time))
-            return self._lines
-        return anim
-
-    def _anim_gamma(self, start, system_gamma_per_frame) -> Callable[[int], List[plt.Line2D]]:
-        self._state.gamma = start
-        self.plot()
-        def anim(i):
-            gamma = start+i*system_gamma_per_frame
-            self._state.gamma = gamma
-            self._lines[0].set_data(self._x, self._y(self._time))
-            return self._lines
-        return anim
-"""
+    def update(self) -> None:
+        self._expectation_value = lambda t: self._state.new_k_space_expectation_value(t)
 
 class New_Momentum_Space_Plot(Single_Updatable_Plot):
     def __init__(self, state: pib.Particle_in_Box_State, mode: str, fig: plt.Figure, axis: plt.Axes=None) -> None:
         super().__init__(state, fig, axis=axis)
         self._mode = mode
-        self._time = 0
         self._bound = 15
         self._bar_color = light_blue
         self._bars = None
 
-    def plot_config(self, bar_color):
+    def plot_config(self, bar_color) -> New_Momentum_Space_Plot:
         self._bar_color = bar_color
         return self
 
-    def set_n_bound(self, new_bound):
+    def set_n_bound(self, new_bound) -> New_Momentum_Space_Plot:
         self._bound = new_bound
-        return self
-
-    def set_t(self, time):
-        self._time = time
         return self
 
     def update(self) -> None:
@@ -285,49 +248,75 @@ class New_Momentum_Space_Plot(Single_Updatable_Plot):
         elif self._mode=="imag":
            self._y = lambda t: np.imag(self._state.new_k_space_wavefunction(self._n, t))
 
+    def _line_data_init(self) -> None:
+        self._bars = self._axis.bar(self._kn, self._y(self._time), self._barwidth, color=self._bar_color, animated=True)
+        self._lines = list(self._bars)
+
+    def _line_data_updater(self) -> None:
+        for bar, h in zip(self._bars, self._y(self._time)):
+            bar.set_height(h)
+        self._lines = list(self._bars)
+
     def plot(self) -> BarContainer:
-        self.update()
-        if self._bars:
-            for bar, h in zip(self._bars, self._y(self._time)):
-                bar.set_height(h)
-        else:
-            self._bars = self._axis.bar(self._kn, self._y(self._time), self._barwidth, color=self._bar_color)
+        super().plot()
         return self._bars
 
-    def _anim_t(self, start, system_time_per_frame) -> Callable[[int], List[plt.Line2D]]:
-        self._time = start
-        self.plot()
-        def anim(i):
-            t = start+i*system_time_per_frame
-            for bar, h in zip(self._bars, self._y(t)):
-                bar.set_height(h)
-            return list(self._bars)
-        return anim
+class Expectation_Value_Evolution(Single_Updatable_Plot):
+    def __init__(self, state: pib.Particle_in_Box_State, fig: plt.Figure, axis: plt.Axes=None) -> None:
+        super().__init__(state, fig, axis=axis)
+        self._t_range = [0,10]
+        self._time_line = None
+        self._time_evolution = None
+        self._value_at_time = None
+        self._c_color = "0.7"
 
-    def _anim_gamma(self, start, system_gamma_per_frame) -> Callable[[int], List[plt.Line2D]]:
-        self._state.gamma = start
-        self.plot()
-        def anim(i):
-            pass
+    def plot_config(self, color=dark_blue, linestyle="-", linewidth=1, c_color="0.7"):
+        self._c_color = c_color
+        return super().plot_config(color=color, linestyle=linestyle, linewidth=linewidth)
+        
+    def set_t_range(self, new_range):
+        self._t_range = new_range
+        return self
 
+    def update(self) -> None:
+        self._t = np.linspace(self._t_range[0], self._t_range[1], self._res)
+        self._expectation_value = None
+
+    def _line_data_init(self):
+        self._time_evolution = self._axis.plot(self._t, self._expectation_value(self._t), color=self._color, linewidth=self._lw, linestyle=self._ls, animated=True)
+        self._time_line = self._axis.axvline(self._time, color=self._c_color, animated=True)
+        self._value_at_time = self._axis.axhline(self._expectation_value(self._time), color=self._c_color, animated=True)
+        self._lines = self._time_evolution + [self._time_line] + [self._value_at_time]
+
+    def _line_data_updater(self):
+        self._time_evolution[0].set_data(self._t, self._expectation_value(self._t))
+        self._time_line.set_data([self._time, self._time],[0,1])
+        expectation_value = self._expectation_value(self._time)
+        self._value_at_time.set_data([1,0],[expectation_value, expectation_value])
+        self._lines = self._time_evolution + [self._time_line] + [self._value_at_time]
     
+class Position_Expectation_Value_Evolution(Expectation_Value_Evolution):
+    def __init__(self, state: pib.Particle_in_Box_State, fig: plt.Figure, axis: plt.Axes=None) -> None:
+        super().__init__(state, fig, axis=axis)
+
+    def update(self) -> None:
+        self._t = np.linspace(self._t_range[0], self._t_range[1], self._res)
+        self._expectation_value = lambda t: self._state.x_space_expectation_value(t)
+
+class Momentum_Expectation_Value_Evolution(Expectation_Value_Evolution):
+    def __init__(self, state: pib.Particle_in_Box_State, fig: plt.Figure, axis: plt.Axes=None) -> None:
+        super().__init__(state, fig, axis=axis)
+
+    def update(self) -> None:
+        self._t = np.linspace(self._t_range[0], self._t_range[1], self._res)
+        self._expectation_value = lambda t: self._state.new_k_space_expectation_value(t)
 
 class Update_Plot_Collection(Updatable_Plot):
     def __init__(self, fig, *plots: Single_Updatable_Plot) -> None:
         super().__init__(fig)
         self._plots = plots
 
-    def _anim_generic(self, var, start, system_var_per_frame):
-        anim_list = []
-        if var=="t":
-            for plot in self._plots:
-                anim_list.append(plot._anim_t(start, system_var_per_frame))
-        elif var=="L":
-            for plot in self._plots:
-                anim_list.append(plot._anim_L(start, system_var_per_frame))
-        elif var=="gamma":
-            for plot in self._plots:
-                anim_list.append(plot._anim_gamma(start, system_var_per_frame))
+    def _anim_generic(self, anim_list):
         def anim(i):
             artists = anim_list[0](i)
             for plot_index in range(1, len(anim_list)):
@@ -335,15 +324,28 @@ class Update_Plot_Collection(Updatable_Plot):
             return artists
         return anim
             
-
     def _anim_t(self, start, system_time_per_frame) -> Callable[[int], List[plt.Line2D]]:
-        return self._anim_generic("t", start, system_time_per_frame)
+        anim_list = []
+        for plot in self._plots:
+            anim_list.append(plot._anim_t(start, system_time_per_frame))
+        return self._anim_generic(anim_list)
 
     def _anim_L(self, start, system_L_per_frame) -> Callable[[int], List[plt.Line2D]]:
-        return self._anim_generic("L", start, system_L_per_frame)
+        anim_list = []
+        for plot in self._plots:
+            anim_list.append(plot._anim_L(start, system_L_per_frame))
+        return self._anim_generic(anim_list)
 
     def _anim_gamma(self, start, system_gamma_per_frame) -> Callable[[int], List[plt.Line2D]]:
-        return self._anim_generic("gamma", start, system_gamma_per_frame)
+        anim_list = []
+        for plot in self._plots:
+            anim_list.append(plot._anim_L(start, system_gamma_per_frame))
+        return self._anim_generic(anim_list)
+
+    def set_t(self, time) -> Update_Plot_Collection:
+        for plot in self._plots:
+            plot.set_t(time)
+        return self
 
     def plot(self):
         for plot in self._plots:
